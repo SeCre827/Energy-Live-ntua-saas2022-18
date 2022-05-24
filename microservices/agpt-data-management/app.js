45ec20e98a6ffed0bb9a2d1b87547e5cabb6beeb
Stefanos Tsolos <43885015+stefanostsolos@users.noreply.github.com>
2022-05-24 15:02:26 +0300

    Add files via upload

--- microservices/agpt-data-management/app.js
+++ microservices/agpt-data-management/app.js
@@ -1,8 +1,7 @@
 const createError = require('http-errors');
 const express = require('express');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const cors = require('cors')
 const indexRouter = require('./routes/index');
 
 const app = express();
@@ -10,7 +9,7 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(cors());
 
 app.use('/', indexRouter);
 
